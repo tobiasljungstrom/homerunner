f6d8d23b8e11e2c238963ee99eac548e1286ee9c
Tobias <tobias.ljungstrom@gmail.com>
2017-12-19 10:52:43 +0100

    Add support for loading styles, create ground

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,17 @@
+import './styles/main.css'
+
 const homerunner = function (baseElement) {
-  console.log('element', baseElement)
+  const mainCanvas = document.createElement('div')
+  mainCanvas.className = 'homerunner-main-canvas'
+  baseElement.appendChild(mainCanvas)
+
+  createGround(mainCanvas)
+}
+
+function createGround (container) {
+  const groundDiv = document.createElement('div')
+  groundDiv.className = 'homerunner-ground'
+  container.appendChild(groundDiv)
 }
 
 export default homerunner
