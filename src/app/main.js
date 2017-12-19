61912a589625d4eb7f188c912676feaa5b64fbdd
Tobias <tobias.ljungstrom@gmail.com>
2017-12-19 13:45:12 +0100

    Add obstacles

--- src/app/main.js
+++ src/app/main.js
@@ -2,13 +2,16 @@ import './styles/main.css'
 
 const GROUND_HEIGHT = 100
 const ARROW_UP = 38
+const JUMP_DURATION = 600
+const JUMP_HEIGHT = 150
 
 // Elements
 let player = null
 
 // State
-let isJumping = false
 let currentScore = 0
+let latestJump = 0
+let obstacles = []
 
 const homerunner = function (baseElement) {
   const mainCanvas = document.createElement('div')
@@ -47,12 +50,14 @@ function createCounter (container) {
 function start () {
   listenForInput()
   countScore()
+  spawnObstacles()
+  window.setInterval(mainLoop, 10)
 }
 
 function listenForInput () {
-  window.addEventListener('keyup', (key) => {
+  window.addEventListener('keydown', (key) => {
     if (key.keyCode === ARROW_UP) {
-      if (!isJumping) {
+      if (canJump()) {
         jump()
       }
     }
@@ -60,12 +65,18 @@ function listenForInput () {
 }
 
 function jump () {
-  player.style.transform = 'translateY(-100px)'
-  isJumping = true
+  latestJump = +new Date()
+  player.style.transitionTimingFunction = 'ease-out'
+  player.style.transform = `translateY(-${JUMP_HEIGHT}px)`
   setTimeout(() => {
+    player.style.transitionTimingFunction = 'ease-in'
     player.style.transform = 'translateY(0px)'
-    isJumping = false
-  }, 250)
+  }, JUMP_DURATION / 2)
+}
+
+function canJump () {
+  const now = +new Date()
+  return now - latestJump > JUMP_DURATION
 }
 
 function countScore () {
@@ -77,6 +88,38 @@ function countScore () {
 
 function updateScore () {
   document.querySelector('.homerunner-counter').innerHTML = currentScore
+}
+
+function spawnObstacles () {
+  window.setInterval(spawnObstacle, 1000)
+}
+
+function spawnObstacle () {
+  const obstacleDiv = document.createElement('div')
+  obstacleDiv.className = 'homerunner-obstacle'
+  document.querySelector('.homerunner-main-canvas').appendChild(obstacleDiv)
+  obstacles.push([obstacleDiv, 0])
+}
+
+function mainLoop () {
+  obstacles.forEach(obstacle => {
+    const currentPos = obstacle[1]
+    const newPos = currentPos + 4
+    obstacle[0].style.right = `${newPos}px`
+    obstacle[1] = newPos
+  })
+
+  obstacles.forEach(obstacle => {
+    const element = obstacle[0]
+    const offset = element.style.right.substr(0, element.style.right.length - 2)
+    const windowWidth = document.querySelector('.homerunner-main-canvas').offsetWidth
+    if (offset > windowWidth) {
+      console.log(obstacles)
+      element.parentElement.removeChild(element)
+      obstacles.splice(obstacles.indexOf(obstacle), 1)
+      console.log(obstacles)
+    }
+  })
 }
 
 export default homerunner
